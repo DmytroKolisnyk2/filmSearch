3948b48d68ddb568512b8f681b107890e9892cbe
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-09-19 13:38:58 +0300

    add icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-// import 'material-design-icons/iconfont/material-icons.css';
+import 'material-design-icons/iconfont/material-icons.css';
 import { openBar } from './js/openBar';
 
 const burgerRef = document.querySelector('.header__burger');

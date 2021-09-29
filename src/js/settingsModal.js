5a267b55e26cd98f3d28428b3ea6120d8a3ed6ca
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-09-29 20:19:57 +0300

    add lang change

--- src/js/settingsModal.js
+++ src/js/settingsModal.js
@@ -2,17 +2,29 @@ import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import settingsTmpl from '../templates/settings.hbs';
 import { searchCountries, searchLang } from './searchFilm';
+import { changeAxiosLanguage, changeAxiosRegion } from './searchFilm';
 
 export const openSettings = () => {
   try {
-    const instance = basicLightbox.create(`<div><img src="./images/loader.gif" alt="loader"></div>`);
-    instance.show()
-    searchCountries().then((data) => {
-      searchLang().then((langData) => {
+    const instance = basicLightbox.create(
+      `<div><img src="./images/loader.gif" alt="loader"></div>`,
+    );
+    instance.show();
+    searchCountries().then(data => {
+      searchLang().then(langData => {
         data.langData = langData.data;
         document.querySelector('.basicLightbox__placeholder').innerHTML = settingsTmpl(data);
-      })
-    })
+        document.querySelector('#language').value = localStorage.getItem('language');
+        document
+          .querySelector('#language')
+          .addEventListener('change', event => changeAxiosLanguage(event.target.value));
+        document.querySelector('#region').value = localStorage.getItem('region');
+        document
+          .querySelector('#region')
+          .addEventListener('change', event => changeAxiosRegion(event.target.value));
+      });
+    });
+  } catch {
+    () => alert('Opps something went wrong');
   }
-  catch { () => alert("Opps something went wrong") }
-}
+};

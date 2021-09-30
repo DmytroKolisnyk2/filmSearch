b52dc79c5d1b7c5cbfc85f843585d6b865165477
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-09-30 22:16:01 +0300

    add change theme func

---
+++ src/js/changeStyle.js
@@ -0,0 +1,52 @@
+export const themeStyles = {
+  defaultTheme: {
+    // name: 'default',
+    '--headline-white': '#FEFFF1',
+    '--aside-bg': '#4c4145',
+    '--headline-bg': '#3a2f33',
+    '--text-grey': '#C7C7C7',
+    '--rating-color': '#fcd46b',
+  },
+  greenTheme: {
+    // name: 'green',
+    '--headline-white': '#d5e2ed',
+    '--aside-bg': '#bad072',
+    '--headline-bg': '#6e9a44',
+    '--text-grey': '#C7C7C7',
+    '--rating-color': '#3d9690',
+  },
+  pinkTheme: {
+    // name: 'pink',
+    '--headline-white': '#FEFFF1',
+    '--aside-bg': '#f5a3b7',
+    '--headline-bg': '#715f65',
+    '--text-grey': '#C7C7C7',
+    '--rating-color': '#fcd46b',
+  },
+  pinkGreenTheme: {
+    // name: 'pink-green',
+    '--headline-white': '#fee2e3',
+    '--aside-bg': '#9db802',
+    '--headline-bg': '#025b0e',
+    '--text-grey': '#C7C7C7',
+    '--rating-color': '#fd7c84',
+  },
+  yellowTheme: {
+    // name: 'yellow',
+    '--headline-white': '#FEFFF1',
+    '--aside-bg': '#ddce6e',
+    '--headline-bg': '#e9cb31',
+    '--text-grey': '#d9d9c2',
+    '--rating-color': '#676778',
+  },
+};
+
+export const changeTheme = (theme) => {
+  console.log(themeStyles[theme]);
+  localStorage.setItem('theme', theme);
+  // themeStyles.theme  
+  for (const elem in themeStyles[theme]) {
+    document.documentElement.style.setProperty(elem, themeStyles[theme][elem]);
+
+  }
+};

763c91428e9d467fd95c8e3b2e5a488ac443a5e6
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-10-02 00:38:08 +0300

    change theme color

--- src/js/changeStyle.js
+++ src/js/changeStyle.js
@@ -1,65 +1,83 @@
 export const themeStyles = {
-  defaultTheme: {
-    // name: 'default',
+  'Default theme': {
     '--headline-white': '#FEFFF1',
     '--aside-bg': '#4c4145',
     '--headline-bg': '#3a2f33',
     '--text-grey': '#C7C7C7',
     '--rating-color': '#fcd46b',
-    '--main-color':'#ededed'
+    '--main-color': '#ededed',
+    '--gear-bg': '#4c4145',
+    '--text-color-dark': '#3a2f33',
+    '--text-color': '#4c4145',
+    '--gear-inner': '#FEFFF1',
   },
-  greenTheme: {
-    // name: 'green',
-    '--headline-white': '#023026',
-    '--aside-bg': '#bad072',
-    '--headline-bg': '#6e9a44',
-    '--text-grey': '#023026',
-    '--rating-color': '#3d9690',
-    '--main-color':'#ededed'
-  },
-  pinkTheme: {
-    // name: 'pink',
-    '--headline-white': '#023026',
-    '--aside-bg': '#f5a3b7',
-    '--headline-bg': '#715f65',
-    '--text-grey': '#023026',
-    '--rating-color': '#fcd46b',
-    '--main-color':'#ededed'
-  },
-  pinkGreenTheme: {
-    // name: 'pink-green',
-    '--headline-white': '#fee2e3',
-    '--aside-bg': '#9db802',
-    '--headline-bg': '#025b0e',
-    '--text-grey': '#023026',
-    '--rating-color': '#fd7c84',
-    '--main-color':'#ededed'
-  },
-  yellowTheme: {
-    // name: 'yellow',
-    '--headline-white': '#444444',
-    '--aside-bg': '#fcd46b',
-    '--headline-bg': '#ddce6e',
-    '--text-grey': '#676778',
-    '--rating-color': '#272933',
-    '--main-color': '#d9d9c2'
-  }, darkTheme: {
-    // name: 'yellow',
+  'Dark theme': {
     '--headline-white': '#dcdcdc',
-    '--aside-bg': '#777',
-    '--headline-bg': '#606060',
+    '--aside-bg': '#404040',
+    '--headline-bg': '#494949',
     '--text-grey': '#bbb',
     '--rating-color': '#fcd46b',
-    '--main-color': '#333333'
+    '--main-color': '#333333',
+    '--gear-bg': '#efefef',
+    '--gear-inner': '#333333',
+    '--text-color-dark': '#acacac',
+    '--text-color': '#dcdcdc',
+  },
+  'Casual theme': {
+    '--headline-white': '#5c4f55',
+    '--aside-bg': '#F9F8D4',
+    '--headline-bg': '#E7C06E',
+    '--text-grey': '#4c4145',
+    '--rating-color': '#34BEEA',
+    '--main-color': '#F9E3AF',
+    '--gear-bg': '#4c4145',
+    '--text-color-dark': '#3a2f33',
+    '--text-color': '#4c4145',
+    '--gear-inner': '#FEFFF1',  
+  },
+  'Green theme': {
+    '--headline-white': '#d5e2ed',
+    '--aside-bg': '#6e9a44',
+    '--headline-bg': '#02302699',
+    '--text-grey': '#d5e2ed',
+    '--rating-color': '#d5e2ed',
+    '--main-color': '#bad072',
+    '--gear-inner': '#FEFFF1',
+    '--gear-bg': '#6e9a44',
+    '--text-color-dark': '#3a2f33',
+    '--text-color': '#4c4145',
+  },
+  'Pink theme': {
+    '--headline-white': '#023026',
+    '--aside-bg': '#f5a3b7',
+    '--headline-bg': '#BFB9CF',
+    '--text-grey': '#715f65',
+    '--rating-color': '#715f65',
+    '--main-color': '#fee2e3',
+    '--gear-inner': '#FEFFF1',
+    '--gear-bg': '#BFB9CF',
+    '--text-color-dark': '#3a2f33',
+    '--text-color': '#4c4145',
+  },
+  'Yellow theme': {
+    '--headline-white': '#272933',
+    '--headline-bg': '#fcd46b',
+    '--aside-bg': '#ddce6e',
+    '--text-grey': '#272933',
+    '--rating-color': '#272933',
+    '--main-color': '#d9d9c2',
+    '--gear-inner': '#ddce6e',
+    '--gear-bg': '#272933',
+    '--text-color-dark': '#272933',
+    '--text-color': '#676778',
   },
 };
 
-export const changeTheme = (theme) => {
+export const changeTheme = theme => {
   console.log(themeStyles[theme]);
   localStorage.setItem('theme', theme);
-  // themeStyles.theme  
+  // themeStyles.theme
   for (const elem in themeStyles[theme]) {
     document.documentElement.style.setProperty(elem, themeStyles[theme][elem]);
-
   }
 };
